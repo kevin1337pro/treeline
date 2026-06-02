bc49264ff45737a8f763fb602241b28de65e2ae3
kevin1337pro <realgoat31337@gmail.com>
2026-06-02 13:02:11 +0200

    Configure Appwrite project

--- components/appwrite.config.js
+++ components/appwrite.config.js
@@ -1,1 +1,16 @@
-window.TREELINE_APPWRITE_CONFIG = null;
+window.TREELINE_APPWRITE_CONFIG = {
+  endpoint: "https://fra.cloud.appwrite.io/v1",
+  projectId: "6a1eb74900004d821d08",
+  databaseId: "treeline",
+  treesCollectionId: "trees",
+  collectionIds: {
+    trees: "trees",
+    orders: "orders",
+    measures: "measures",
+    users: "users",
+    vehicles: "vehicles",
+    equipment: "equipment",
+    plantings: "plantings",
+    media: "media",
+  },
+};
